91bfd3cc960311e6cc1118b43ca4bedbab555308
10147243 <10101472343@georgebrown.ca>
2024-04-07 23:13:19 -0400

    assignment todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todo, setTodo] = useState([]);
+	const [userID, setUserID] = useState(1);
+	const [fetching, setFetching] = useState(false);
+  const handleChange = (e) => {
+    setUserID(e.target.value);
+  };
+  
+	useEffect(() => {
+		setFetching(true);
+		console.log("test");
+		fetch(`https://dummyjson.com/todos/user/${userID}`)
+			.then((res) => res.json())
+			.then((data) => {
+				setTodo(data.todos);
+				setFetching(false);
+			});
+	}, [userID]);
+
+	return (
+		<section>
+			<header>
+				<h1>Todos</h1>
+			</header>
+			<div>
+				<label htmlFor="user">Please select an user</label>
+				<select id="user" onChange={handleChange}>
+					<option value="1">Arthur</option>
+					<option value="2">Sarah</option>
+					<option value="3">Max</option>
+				</select>
+			</div>
+			<main>
+				{fetching ? (
+					<p>Loading Data</p>
+				) : (
+					<ul>
+						{todo.map((task) => {
+							return <li key={task.id}>{task.todo}</li>;
+						})}
+					</ul>
+				)}
+			</main>
+		</section>
+	);
 }
 
 export default App;
